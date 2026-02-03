ab2dbe1df1e882d04c18606d4d27e3fa059c2b48
Ayushmaan Yadav <f20231044@pilani.bits-pilani.ac.in>
2026-02-03 20:14:00 +0530

    Add C++ limit-order-book matching engine (AVL + FIFO) with trade-observer hook

---
+++ engine/OrderPipeline.cpp
@@ -0,0 +1,112 @@
+#include "OrderPipeline.hpp"
+#include "Book.hpp"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <random>
+#include <chrono>
+
+OrderPipeline::OrderPipeline(Book* book) : book(book) {
+    orderFunctions = {
+        {"Market", &OrderPipeline::processMarketOrder},
+        {"AddLimit", &OrderPipeline::processAddLimitOrder},
+        {"AddMarketLimit", &OrderPipeline::processAddLimitOrder},
+        {"CancelLimit", &OrderPipeline::processCancelLimitOrder},
+        {"ModifyLimit", &OrderPipeline::processModifyLimitOrder},
+        {"AddStop", &OrderPipeline::processAddStopOrder},
+        {"CancelStop", &OrderPipeline::processCancelStopOrder},
+        {"ModifyStop", &OrderPipeline::processModifyStopOrder},
+        {"AddStopLimit", &OrderPipeline::processAddStopLimitOrder},
+        {"CancelStopLimit", &OrderPipeline::processCancelStopLimitOrder},
+        {"ModifyStopLimit", &OrderPipeline::processModifyStopLimitOrder}
+    };
+}
+
+void OrderPipeline::processOrdersFromFile(const std::string& filename) 
+{
+    std::ifstream file(filename);
+    if (!file.is_open()) {
+        std::cerr << "Error opening file: " << filename << std::endl;
+        return;
+    }
+
+    std::string line;
+    while (std::getline(file, line)) {
+        std::istringstream iss(line);
+        std::string orderType;
+        iss >> orderType;
+
+        auto it = orderFunctions.find(orderType);
+        if (it != orderFunctions.end()) {
+            (this->*(it->second))(iss);
+        } else {
+            std::cerr << "Unknown order type: " << orderType << std::endl;
+        }
+    }
+    file.close();
+}
+
+void OrderPipeline::processMarketOrder(std::istringstream& iss) {
+    int orderId, shares;
+    bool buyOrSell;
+    iss >> orderId >> buyOrSell >> shares;
+    book->marketOrder(orderId, buyOrSell, shares);
+}
+
+void OrderPipeline::processAddLimitOrder(std::istringstream& iss) {
+    int orderId, shares, limitPrice;
+    bool buyOrSell;
+    iss >> orderId >> buyOrSell >> shares >> limitPrice;
+    book->addLimitOrder(orderId, buyOrSell, shares, limitPrice);
+}
+
+void OrderPipeline::processCancelLimitOrder(std::istringstream& iss) {
+    int orderId;
+    iss >> orderId;
+    book->cancelLimitOrder(orderId);
+}
+
+void OrderPipeline::processModifyLimitOrder(std::istringstream& iss) {
+    int orderId, newShares, newLimit;
+    iss >> orderId >> newShares >> newLimit;
+    book->modifyLimitOrder(orderId, newShares, newLimit);
+}
+
+void OrderPipeline::processAddStopOrder(std::istringstream& iss) {
+    int orderId, shares, stopPrice;
+    bool buyOrSell;
+    iss >> orderId >> buyOrSell >> shares >> stopPrice;
+    book->addStopOrder(orderId, buyOrSell, shares, stopPrice);
+}
+
+void OrderPipeline::processCancelStopOrder(std::istringstream& iss) {
+    int orderId;
+    iss >> orderId;
+    book->cancelStopOrder(orderId);
+}
+
+void OrderPipeline::processModifyStopOrder(std::istringstream& iss) {
+    int orderId, newShares, newStopPrice;
+    iss >> orderId >> newShares >> newStopPrice;
+    book->modifyStopOrder(orderId, newShares, newStopPrice);
+}
+
+void OrderPipeline::processAddStopLimitOrder(std::istringstream& iss) {
+    int orderId, shares, limitPrice, stopPrice;
+    bool buyOrSell;
+    iss >> orderId >> buyOrSell >> shares >> limitPrice >> stopPrice;
+    book->addStopLimitOrder(orderId, buyOrSell, shares, limitPrice, stopPrice);
+}
+
+void OrderPipeline::processCancelStopLimitOrder(std::istringstream& iss) {
+    int orderId;
+    iss >> orderId;
+    book->cancelStopLimitOrder(orderId);
+}
+
+void OrderPipeline::processModifyStopLimitOrder(std::istringstream& iss) {
+    int orderId, newShares, newLimitPrice, newStopPrice;
+    iss >> orderId >> newShares >> newLimitPrice >> newStopPrice;
+    book->modifyStopLimitOrder(orderId, newShares, newLimitPrice, newStopPrice);
+}
